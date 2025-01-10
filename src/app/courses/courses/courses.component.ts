b2dda0226f96d75c588c8ac88110175bc84211ec
Marcosdsj <mfsilva5585@gmail.com>
2025-01-09 21:24:48 -0300

    table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [] // pode iniciar tanto aqui quanto no construtor
+  courses: Course[] = [
+    {_id: "1", name: "Java", category:"Back-end"}
+  ]; // pode iniciar tanto aqui quanto no construtor
+  displayedColumns = ['name', 'category'];
 
   constructor(){
     //this.courses = [];
