04da0d6902acac056dc42c2a938f9d24db87a7aa
Marcosdsj <mfsilva5585@gmail.com>
2025-01-10 21:39:36 -0300

    add toolbar

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+   //CoursesComponent
+  ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { MatTableModule } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-courses',
   imports: [
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
