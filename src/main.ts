7b34658136bfd475468142d54fe9cfa2cc6d2f53
Marcosdsj <mfsilva5585@gmail.com>
2025-01-14 20:59:21 -0300

    atuali

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
+//import {APP_ROUTES} from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, MatToolbarModule, HttpClient),
+    provideAnimations(),
+    //provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(),
+    //provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
+  ]
+})
   .catch((err) => console.error(err));
